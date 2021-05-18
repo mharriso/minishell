0b8a9b512c96616b53fc2fcbb2d3790d3f543199
Julia <63446729+mharriso@users.noreply.github.com>
2021-05-18 14:49:42 +0300

    Update ft_lstnew.c

--- libft/ft_lstnew.c
+++ libft/ft_lstnew.c
@@ -12,13 +12,13 @@
 
 #include "libft.h"
 
-t_list	*ft_lstnew(void *data)
+t_list	*ft_lstnew(void *content)
 {
 	t_list *new;
 
 	if ((new = (t_list *)malloc(sizeof(t_list))))
 	{
-		new->data = data;
+		new->content = content;
 		new->next = NULL;
 	}
 	return (new);
